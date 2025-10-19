92c11e77fcefd368101382d4e90f1b887f8bae7f
ataklti <atak2891@gmail.com>
2025-10-19 13:48:05 +0300

    Register student, course and attendance in admin

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Student, Course, Attendance
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'email', 'phone', 'date_joined')
+    search_fields = ('full_name', 'email')
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date')
+    search_fields = ('title',)
+
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('student', 'course', 'date', 'status')
+    list_filter = ('status', 'course')
+    search_fields = ('student__full_name', 'course__title')
