52ac651b6476a390810664831608d65c078bb9c9
ataklti <atak2891@gmail.com>
2025-10-19 13:37:20 +0300

    Add search and ordering filters for attendance by course title and date

--- students/views.py
+++ students/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Student, Course, Attendance
 from .serializers import StudentSerializer, CourseSerializer, AttendanceSerializer
 
@@ -6,10 +6,15 @@ class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+
 class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['course__title', 'date']
+    ordering_fields = ['date']
